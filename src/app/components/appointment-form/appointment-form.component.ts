ba58b214726cde2a6ed17fc72b683bc222682bf8
Aliyah Williams <137098728+A-Dezeray@users.noreply.github.com>
2025-05-09 15:32:55 -0400

    Added in logic for the form and connected appointmentService

--- src/app/components/appointment-form/appointment-form.component.ts
+++ src/app/components/appointment-form/appointment-form.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppointmentService } from '../../services/appointment.service';
 
 @Component({
   selector: 'app-appointment-form',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './appointment-form.component.html',
-  styleUrl: './appointment-form.component.css'
+  styleUrls: ['./appointment-form.component.css']
 })
 export class AppointmentFormComponent {
+  name = '';
+  date = '';
+  time = '';
+  description = '';
 
+  constructor(private appointmentService: AppointmentService) {}
+
+  addAppointment() {
+    if (this.name && this.date && this.time && this.description) {
+      this.appointmentService.addAppointment({
+        name: this.name,
+        date: this.date,
+        time: this.time,
+        description: this.description
+      });
+
+      // Reset form
+      this.name = '';
+      this.date = '';
+      this.time = '';
+      this.description = '';
+    }
+  }
 }
