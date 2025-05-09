5cafb5b62f59fb3558f2a7be80a2fd7632f3fb96
Aliyah Williams <137098728+A-Dezeray@users.noreply.github.com>
2025-05-09 15:41:40 -0400

    Updated to display appointments dynamically in list component

--- src/app/components/appointment-list/appointment-list.component.ts
+++ src/app/components/appointment-list/appointment-list.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppointmentService } from '../../services/appointment.service';
+import { Appointment } from '../../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.css'
+  styleUrls: ['./appointment-list.component.css']
 })
 export class AppointmentListComponent {
+  appointments: Appointment[] = [];
 
+  constructor(private appointmentService: AppointmentService) {}
+
+  ngOnInit() {
+    this.appointments = this.appointmentService.getAppointments();
+  }
 }
